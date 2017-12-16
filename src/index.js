02fa66874f3ba8240422da1fe4aa302507d96e1b
Anirudh Srinivas <sanirudh94@gmail.com>
2017-12-16 20:38:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
+
+ReactDOM.render(
+  <CommentBox
+    url='http://localhost:3001/api/comments' />,
+  document.getElementById('root')
+);
